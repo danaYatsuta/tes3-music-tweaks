478b7fbe43e8fb7eb966146cc89d7af9bd2c74ae
Dana Yatsuta <danayatsuta@gmail.com>
2026-05-10 17:53:20 +0300

    Define music states

--- MWSE/mods/Music Tweaks/main.lua
+++ MWSE/mods/Music Tweaks/main.lua
@@ -1,3 +1,14 @@
+-- Music states
+local EXPLORE = 0
+local COMBAT = 1
+local PAUSE = 2
+local DUNGEON = 3
+-- Title music, level up, death, etc
+local OTHER = 4
+
+-- Current music state. Starts out as OTHER because the game begins at main menu
+local musicState = OTHER
+
 local function initialized()
 	print("[Music Tweaks: INFO] Music Tweaks Initialized")
 end
