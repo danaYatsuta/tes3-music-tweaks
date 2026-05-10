4ad88bae389d09839a4e12d56eb2a431413e4950
Dana Yatsuta <danayatsuta@gmail.com>
2026-05-10 18:44:06 +0300

    Set state on game load

--- MWSE/mods/Music Tweaks/main.lua
+++ MWSE/mods/Music Tweaks/main.lua
@@ -10,10 +10,43 @@ local DUNGEON = 3
 -- Title music, level up, death, etc
 local OTHER = 4
 
+local musicStates = { EXPLORE, COMBAT, PAUSE, DUNGEON, OTHER }
+
 -- Current music state. Starts out as OTHER because the game begins at main menu
+-- Should NEVER be written to outside of setState function
 local musicState = OTHER
 
+-- Should NEVER be called outside of stateExplore/stateCombat/etc functions 
+local function setMusicState(newMusicState)
+	if not musicStates[newMusicState] then
+		return
+	end
+
+	tes3.messageBox("New music state: " .. musicState)
+	musicState = newMusicState
+end
+
+local function statePause()
+	setMusicState(PAUSE)
+end
+
+local function stateDungeon()
+	setMusicState(DUNGEON)
+end
+
+--- @param e cellChangedEventData
+local function cellChangedCallback(e)
+	if musicState == OTHER then
+		if (e.cell.isOrBehavesAsExterior or e.cell.restingIsIllegal) then
+			statePause()
+		else
+			stateDungeon()
+		end
+	end
+end
+
 local function initialized()
+	event.register(tes3.event.cellChanged, cellChangedCallback)
 	print("[Music Tweaks: INFO] Music Tweaks Initialized")
 end
 
